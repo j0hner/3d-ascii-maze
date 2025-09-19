cbaf2941f4d5d890d819111b068f6b76807a5934
j0hner <honner.jan2@gmail.com>
2025-09-19 14:57:56 +0200

    started implementig prims algorythm got bool array initialized

--- maze traveler/Program.cs
+++ maze traveler/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Prims(10);
+    }
+
+    static bool[,] Prims(int sideLength)
+    {
+        int length = sideLength * 2 + 2;
+        bool[,] maze = new bool[length, length];
+        for (int y = 0; y < length - 1; y++)
+        {
+            for (int x = 0; x < length - 1; x++)
+            {
+                maze[y, x] = y % 2 != 0 && x % 2 != 0;
+                Console.Write(maze[y, x] ? "1" : "0");
+            }
+            Console.WriteLine();
+        }
+
+        return new bool[,] { }; //shut up compiler
+    }
+}
